8044c6ebc264273dffa33761fb05634553436a38
Himanshu <himanshu@Himanshus-MacBook-Air.local>
2026-08-26 14:20:07 +0530

    fix: import useMemo from react in EmployerOffers.jsx

--- src/pages/employer/offers/EmployerOffers.jsx
+++ src/pages/employer/offers/EmployerOffers.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import axios from 'axios';
 import { Link, useSearchParams } from 'react-router-dom';
 import {
